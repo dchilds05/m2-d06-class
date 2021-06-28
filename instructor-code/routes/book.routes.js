1af18f9a9c4d1447db6167d3fce2f7f02ffa029e
marco-ih-bcn <marco.santonastasi@ironhack.com>
2021-06-28 13:17:04 +0200

    first route to get and create a book

--- instructor-code/routes/book.routes.js
+++ instructor-code/routes/book.routes.js
@@ -2,7 +2,7 @@
 // APP NEEDS TO KNOW YOU CREATED A NEW ROUTE FILE, THAT'S THE ONLY WAY FOR IT TO KNOW WHICH ROUTES YOU WANT TO HIT
 
 const express = require('express');
-const router = express.Router();
+const router = express.Router(); // This is the Router class inside the Express library
 
 // ********* require Book model in order to use it *********
 const Book = require('../models/Book.model');
@@ -11,10 +11,26 @@ const Book = require('../models/Book.model');
 // GET route to display all the books
 // ****************************************************************************************
 
-router.get('/books', (req, res) => {
+router.get('/', (req, res) => { // What URL does this answer?
   Book.find()
+  .then(allBooks => {
+    console.log(allBooks)
+    res.render('books', {allBooks})})
    // You have to continue coding the route
 });
+
+// This answers to the /books/add URL
+router.post('/add', (req, res)=>{
+  const title = req.body.title
+  const description = req.body.description
+  const author = req.body.author
+  const rating = req.body.rating
+
+  Book.create( {title, description, author, rating} )
+  .then( createdBook => console.log(createdBook))
+})
+
+
 
 // ****************************************************************************************
 // GET route for displaying the book details page
