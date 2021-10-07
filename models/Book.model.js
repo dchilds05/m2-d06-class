b04aee6bbd5e91032289fcf4291d170a34b7cd9c
dechilds1@gmail.com <dechilds1@gmail.com>
2021-10-07 09:36:05 +0200

    last push from mac

--- models/Book.model.js
+++ models/Book.model.js
@@ -5,7 +5,7 @@ const bookSchema = new Schema(
   {
     title: String,
     description: String,
-    author: String,
+    author: { type: Schema.Types.ObjectId, ref: "Author" },
     rating: Number
   },
   {
